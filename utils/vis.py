9af197daf070b5d6f5d755039f7fcb2853bd9a86
Achal Dave <achalddave@live.com>
2018-06-07 14:17:38 +0000

    Remove keypoint visualization, cleanup vis.py

--- utils/vis.py
+++ utils/vis.py
@@ -15,24 +15,16 @@
 
 """Detection output visualization module."""
 
-
-
-
-
-
 import cv2
 import numpy as np
 import os
 
 import pycocotools.mask as mask_util
 
-from utils.colormap import colormap
-import utils.env as envu
-import utils.keypoints as keypoint_utils
+from utils.colors import colormap
 
 # Matplotlib requires certain adjustments in some environments
 # Must happen before importing matplotlib
-envu.set_up_matplotlib()
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 
@@ -42,27 +34,6 @@ plt.rcParams['pdf.fonttype'] = 42  # For editing in Adobe Illustrator
 _GRAY = (218, 227, 218)
 _GREEN = (18, 127, 15)
 _WHITE = (255, 255, 255)
-
-
-def kp_connections(keypoints):
-    kp_lines = [
-        [keypoints.index('left_eye'), keypoints.index('right_eye')],
-        [keypoints.index('left_eye'), keypoints.index('nose')],
-        [keypoints.index('right_eye'), keypoints.index('nose')],
-        [keypoints.index('right_eye'), keypoints.index('right_ear')],
-        [keypoints.index('left_eye'), keypoints.index('left_ear')],
-        [keypoints.index('right_shoulder'), keypoints.index('right_elbow')],
-        [keypoints.index('right_elbow'), keypoints.index('right_wrist')],
-        [keypoints.index('left_shoulder'), keypoints.index('left_elbow')],
-        [keypoints.index('left_elbow'), keypoints.index('left_wrist')],
-        [keypoints.index('right_hip'), keypoints.index('right_knee')],
-        [keypoints.index('right_knee'), keypoints.index('right_ankle')],
-        [keypoints.index('left_hip'), keypoints.index('left_knee')],
-        [keypoints.index('left_knee'), keypoints.index('left_ankle')],
-        [keypoints.index('right_shoulder'), keypoints.index('left_shoulder')],
-        [keypoints.index('right_hip'), keypoints.index('left_hip')],
-    ]
-    return kp_lines
 
 
 def convert_from_cls_format(cls_boxes, cls_segms, cls_keyps):
@@ -88,9 +59,9 @@ def convert_from_cls_format(cls_boxes, cls_segms, cls_keyps):
     return boxes, segms, keyps, classes
 
 
-def get_class_string(class_index, score, dataset):
-    class_text = dataset.classes[class_index] if dataset is not None else \
-        'id{:d}'.format(class_index)
+def get_class_string(class_index, score, class_names):
+    class_text = (class_names[class_index]
+                  if class_names is not None else 'id{:d}'.format(class_index))
     return class_text + ' {:0.2f}'.format(score).lstrip('0')
 
 
@@ -124,83 +95,29 @@ def vis_class(img, pos, class_str, font_scale=0.35):
     cv2.rectangle(img, back_tl, back_br, _GREEN, -1)
     # Show text.
     txt_tl = x0, y0 - int(0.3 * txt_h)
-    cv2.putText(img, txt, txt_tl, font, font_scale, _GRAY, lineType=cv2.LINE_AA)
+    cv2.putText(
+        img, txt, txt_tl, font, font_scale, _GRAY, lineType=cv2.LINE_AA)
     return img
 
 
-def vis_bbox(img, bbox, thick=1):
+def vis_bbox(img, bbox, color=_GREEN, thick=1):
     """Visualizes a bounding box."""
     (x0, y0, w, h) = bbox
     x1, y1 = int(x0 + w), int(y0 + h)
     x0, y0 = int(x0), int(y0)
-    cv2.rectangle(img, (x0, y0), (x1, y1), _GREEN, thickness=thick)
+    cv2.rectangle(img, (x0, y0), (x1, y1), color, thickness=thick)
     return img
 
 
-def vis_keypoints(img, kps, kp_thresh=2, alpha=0.7):
-    """Visualizes keypoints (adapted from vis_one_image).
-    kps has shape (4, #keypoints) where 4 rows are (x, y, logit, prob).
-    """
-    dataset_keypoints, _ = keypoint_utils.get_keypoints()
-    kp_lines = kp_connections(dataset_keypoints)
-
-    # Convert from plt 0-1 RGBA colors to 0-255 BGR colors for opencv.
-    cmap = plt.get_cmap('rainbow')
-    colors = [cmap(i) for i in np.linspace(0, 1, len(kp_lines) + 2)]
-    colors = [(c[2] * 255, c[1] * 255, c[0] * 255) for c in colors]
-
-    # Perform the drawing on a copy of the image, to allow for blending.
-    kp_mask = np.copy(img)
-
-    # Draw mid shoulder / mid hip first for better visualization.
-    mid_shoulder = (
-        kps[:2, dataset_keypoints.index('right_shoulder')] +
-        kps[:2, dataset_keypoints.index('left_shoulder')]) / 2.0
-    sc_mid_shoulder = np.minimum(
-        kps[2, dataset_keypoints.index('right_shoulder')],
-        kps[2, dataset_keypoints.index('left_shoulder')])
-    mid_hip = (
-        kps[:2, dataset_keypoints.index('right_hip')] +
-        kps[:2, dataset_keypoints.index('left_hip')]) / 2.0
-    sc_mid_hip = np.minimum(
-        kps[2, dataset_keypoints.index('right_hip')],
-        kps[2, dataset_keypoints.index('left_hip')])
-    nose_idx = dataset_keypoints.index('nose')
-    if sc_mid_shoulder > kp_thresh and kps[2, nose_idx] > kp_thresh:
-        cv2.line(
-            kp_mask, tuple(mid_shoulder), tuple(kps[:2, nose_idx]),
-            color=colors[len(kp_lines)], thickness=2, lineType=cv2.LINE_AA)
-    if sc_mid_shoulder > kp_thresh and sc_mid_hip > kp_thresh:
-        cv2.line(
-            kp_mask, tuple(mid_shoulder), tuple(mid_hip),
-            color=colors[len(kp_lines) + 1], thickness=2, lineType=cv2.LINE_AA)
-
-    # Draw the keypoints.
-    for l in range(len(kp_lines)):
-        i1 = kp_lines[l][0]
-        i2 = kp_lines[l][1]
-        p1 = kps[0, i1], kps[1, i1]
-        p2 = kps[0, i2], kps[1, i2]
-        if kps[2, i1] > kp_thresh and kps[2, i2] > kp_thresh:
-            cv2.line(
-                kp_mask, p1, p2,
-                color=colors[l], thickness=2, lineType=cv2.LINE_AA)
-        if kps[2, i1] > kp_thresh:
-            cv2.circle(
-                kp_mask, p1,
-                radius=3, color=colors[l], thickness=-1, lineType=cv2.LINE_AA)
-        if kps[2, i2] > kp_thresh:
-            cv2.circle(
-                kp_mask, p2,
-                radius=3, color=colors[l], thickness=-1, lineType=cv2.LINE_AA)
-
-    # Blend the keypoints.
-    return cv2.addWeighted(img, 1.0 - alpha, kp_mask, alpha, 0)
-
-
-def vis_one_image_opencv(
-        im, boxes, segms=None, keypoints=None, thresh=0.9, kp_thresh=2,
-        show_box=False, dataset=None, show_class=False):
+def vis_one_image_opencv(im,
+                         boxes,
+                         segms=None,
+                         keypoints=None,
+                         thresh=0.9,
+                         kp_thresh=2,
+                         show_box=False,
+                         class_names=None,
+                         show_class=False):
     """Constructs a numpy array with the detections visualized."""
 
     if isinstance(boxes, list):
@@ -232,7 +149,7 @@ def vis_one_image_opencv(
 
         # show class (off by default)
         if show_class:
-            class_str = get_class_string(classes[i], score, dataset)
+            class_str = get_class_string(classes[i], score, class_names)
             im = vis_class(im, (bbox[0], bbox[1] - 2), class_str)
 
         # show mask
@@ -241,17 +158,22 @@ def vis_one_image_opencv(
             mask_color_id += 1
             im = vis_mask(im, masks[..., i], color_mask)
 
-        # show keypoints
-        if keypoints is not None and len(keypoints) > i:
-            im = vis_keypoints(im, keypoints[i], kp_thresh)
-
     return im
 
 
-def vis_one_image(
-        im, im_name, output_dir, boxes, segms=None, keypoints=None, thresh=0.9,
-        kp_thresh=2, dpi=200, box_alpha=0.0, dataset=None, show_class=False,
-        ext='pdf'):
+def vis_one_image(im,
+                  im_name,
+                  output_dir,
+                  boxes,
+                  segms=None,
+                  keypoints=None,
+                  thresh=0.9,
+                  kp_thresh=2,
+                  dpi=200,
+                  box_alpha=0.0,
+                  class_names=None,
+                  show_class=False,
+                  ext='pdf'):
     """Visual debugging of detections."""
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -260,16 +182,10 @@ def vis_one_image(
         boxes, segms, keypoints, classes = convert_from_cls_format(
             boxes, segms, keypoints)
 
-    dataset_keypoints, _ = keypoint_utils.get_keypoints()
-
     if segms is not None and len(segms) > 0:
         masks = mask_util.decode(segms)
 
     color_list = colormap(rgb=True) / 255
-
-    kp_lines = kp_connections(dataset_keypoints)
-    cmap = plt.get_cmap('rainbow')
-    colors = [cmap(i) for i in np.linspace(0, 1, len(kp_lines) + 2)]
 
     fig = plt.figure(frameon=False)
     fig.set_size_inches(im.shape[1] / dpi, im.shape[0] / dpi)
@@ -280,10 +196,10 @@ def vis_one_image(
 
     if boxes is None or boxes.shape[0] == 0 or max(boxes[:, 4]) < thresh:
         output_name = os.path.basename(im_name) + '.' + ext
-        fig.savefig(os.path.join(output_dir, '{}'.format(output_name)), dpi=dpi)
+        fig.savefig(
+            os.path.join(output_dir, '{}'.format(output_name)), dpi=dpi)
         plt.close('all')
         return
-
 
     # Display in largest to smallest order to reduce occlusion
     areas = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
@@ -307,7 +223,7 @@ def vis_one_image(
         if show_class:
             ax.text(
                 bbox[0], bbox[1] - 2,
-                get_class_string(classes[i], score, dataset),
+                get_class_string(classes[i], score, class_names),
                 fontsize=3,
                 family='serif',
                 bbox=dict(
@@ -337,56 +253,6 @@ def vis_one_image(
                     edgecolor='w', linewidth=1.2,
                     alpha=0.5)
                 ax.add_patch(polygon)
-
-        # show keypoints
-        if keypoints is not None and len(keypoints) > i:
-            kps = keypoints[i]
-            plt.autoscale(False)
-            for l in range(len(kp_lines)):
-                i1 = kp_lines[l][0]
-                i2 = kp_lines[l][1]
-                if kps[2, i1] > kp_thresh and kps[2, i2] > kp_thresh:
-                    x = [kps[0, i1], kps[0, i2]]
-                    y = [kps[1, i1], kps[1, i2]]
-                    line = plt.plot(x, y)
-                    plt.setp(line, color=colors[l], linewidth=1.0, alpha=0.7)
-                if kps[2, i1] > kp_thresh:
-                    plt.plot(
-                        kps[0, i1], kps[1, i1], '.', color=colors[l],
-                        markersize=3.0, alpha=0.7)
-
-                if kps[2, i2] > kp_thresh:
-                    plt.plot(
-                        kps[0, i2], kps[1, i2], '.', color=colors[l],
-                        markersize=3.0, alpha=0.7)
-
-            # add mid shoulder / mid hip for better visualization
-            mid_shoulder = (
-                kps[:2, dataset_keypoints.index('right_shoulder')] +
-                kps[:2, dataset_keypoints.index('left_shoulder')]) / 2.0
-            sc_mid_shoulder = np.minimum(
-                kps[2, dataset_keypoints.index('right_shoulder')],
-                kps[2, dataset_keypoints.index('left_shoulder')])
-            mid_hip = (
-                kps[:2, dataset_keypoints.index('right_hip')] +
-                kps[:2, dataset_keypoints.index('left_hip')]) / 2.0
-            sc_mid_hip = np.minimum(
-                kps[2, dataset_keypoints.index('right_hip')],
-                kps[2, dataset_keypoints.index('left_hip')])
-            if (sc_mid_shoulder > kp_thresh and
-                    kps[2, dataset_keypoints.index('nose')] > kp_thresh):
-                x = [mid_shoulder[0], kps[0, dataset_keypoints.index('nose')]]
-                y = [mid_shoulder[1], kps[1, dataset_keypoints.index('nose')]]
-                line = plt.plot(x, y)
-                plt.setp(
-                    line, color=colors[len(kp_lines)], linewidth=1.0, alpha=0.7)
-            if sc_mid_shoulder > kp_thresh and sc_mid_hip > kp_thresh:
-                x = [mid_shoulder[0], mid_hip[0]]
-                y = [mid_shoulder[1], mid_hip[1]]
-                line = plt.plot(x, y)
-                plt.setp(
-                    line, color=colors[len(kp_lines) + 1], linewidth=1.0,
-                    alpha=0.7)
 
     output_name = os.path.basename(im_name) + '.' + ext
     fig.savefig(os.path.join(output_dir, '{}'.format(output_name)), dpi=dpi)
