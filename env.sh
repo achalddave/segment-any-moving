3b2ace10f1beafc642fe3945a300cb42bc015054
Achal Dave <achalddave@live.com>
2018-08-01 11:27:08 -0400

    Slight cleanup of env.sh script

--- env.sh
+++ env.sh
@@ -1,17 +1,35 @@
+# Usage: . ./env.sh (activate|deactivate)
 # Hacky script to add/remove this directory to PYTHONPATH.
 
-_TRACK_PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-if [[ -n "${PYTHONPATH}" ]] ; then
-    export PYTHONPATH="${_TRACK_PROJECT_DIR}:${PYTHONPATH}"
-else
-    export PYTHONPATH="${_TRACK_PROJECT_DIR}"
-fi
-unset _TRACK_PROJECT_DIR
+function usage() {
+    echo "Usage:"
+    echo ". ./env.sh (activate|deactivate)"
+}
 
-deactivate_track() {
-    _TRACK_PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+if [[ $_ == $0 ]] ; then
+    echo "This script needs to be sourced."
+    usage
+    exit 1
+fi
+
+if [[ "$#" != 1 ]] ; then
+    usage
+    return
+elif [[ "$1" != "activate" ]] && [[ "$1" != "deactivate" ]] ; then
+    usage
+    return
+fi
+
+_TRACK_PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+if [[ "$1" == "activate" ]] ; then
+    if [[ -n "${PYTHONPATH}" ]] ; then
+        export PYTHONPATH="${_TRACK_PROJECT_DIR}:${PYTHONPATH}"
+    else
+        export PYTHONPATH="${_TRACK_PROJECT_DIR}"
+    fi
+    unset _TRACK_PROJECT_DIR
+elif [[ "$1" == "deactivate" ]] ; then
     export PYTHONPATH=${PYTHONPATH//:${_TRACK_PROJECT_DIR}/}
     export PYTHONPATH=${PYTHONPATH//${_TRACK_PROJECT_DIR}/}
-    unset -f deactivate_track
-    unset _TRACK_PROJECT_DIR
-}
+fi
+unset _TRACK_PROJECT_DIR
