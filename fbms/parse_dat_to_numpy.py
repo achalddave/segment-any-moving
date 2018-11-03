0e171e38824e20d3db09abd447962808611547e0
Achal Dave <achalddave@live.com>
2018-11-03 12:39:41 -0400

    Add option to parse directory of dat to numpy

--- fbms/parse_dat_to_numpy.py
+++ fbms/parse_dat_to_numpy.py
@@ -12,13 +12,30 @@ def main():
     parser = argparse.ArgumentParser(
         description=__doc__.split('\n')[0] if __doc__ else '',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--dat-file', required=True)
-    parser.add_argument('--output-numpy', required=True,)
+
+    parser.add_argument(
+        '--dat-dir',
+        type=Path,
+        help=('Contains a .dat file for each sequence, unless '
+              '--dat-files-in-subdirs is specified, in which case it should '
+              'contain a subdirectory for each sequence, which in turn has a '
+              'single dat file.'))
+    parser.add_argument('--dat-file', type=Path)
+    parser.add_argument(
+        '--output',
+        type=Path,
+        required=True,
+        help=('Output directory. If --dat-file is specified, this refers to '
+              'the output file.'))
     parser.add_argument(
         '--groundtruth-dir',
         type=Path,
         required=True,
-        help='Groundtruth dir for this sequence, containing a .dat file.')
+        help=("FBMS groundtruth split directory, containing a subdirectory "
+              "for each sequence, which in turn contain a 'GroundTruth' "
+              "sequence. If --dat-file is specified, this should point to a "
+              "specific sequence's 'GroundTruth' directory."))
+
     parser.add_argument(
         '--label-size-space-separated',
         action='store_true',
@@ -26,20 +43,62 @@ def main():
               'label> and <track size>, but one of the outputs I downloaded '
               'from a method has the two fields separated by a space. This '
               'boolean allows parsing such files.'))
+    parser.add_argument(
+        '--dat-files-in-subdirs', action='store_true',
+        help=('See --dat-dir help for information. Ignored if --dat-file is '
+              'specified.'))
 
     args = parser.parse_args()
-    setup_logging(args.output_numpy + '.log')
+    assert (args.dat_file is None) != (args.dat_dir is None), (
+        'Exactly one of --dat-file or --dat-dir must be specified.')
+
+    assert not args.output.exists()
+    if args.dat_dir is not None:
+        args.output.mkdir(parents=True)
+        setup_logging(args.output / (Path(__file__).name + '.log'))
+    else:
+        setup_logging(args.output + '.log')
+
     logging.info('Args:\n%s' % args)
 
-    groundtruth = FbmsGroundtruth(args.groundtruth_dir)
-    with open(args.dat_file, 'r') as f:
-        tracks_txt = f.read()
+    if args.dat_dir is not None:
+        sequences = []
+        inputs = []
+        if args.dat_files_in_subdirs:
+            for sequence_dir in args.dat_dir.iterdir():
+                if sequence_dir.is_dir():
+                    dat_file = list(sequence_dir.glob('*.dat'))
+                    if len(dat_file) != 1:
+                        raise ValueError('Found %s (!= 1) dat files in %s' %
+                                         (len(dat_file), sequence_dir))
+                    inputs.append(dat_file[0])
+                    sequences.append(sequence_dir.name)
+        else:
+            for sequence_dat in args.dat_dir.glob('*.dat'):
+                sequences.append(sequence_dat.stem)
+                inputs.append(sequence_dat)
+        groundtruths = [
+            args.groundtruth_dir / x / 'GroundTruth'
+            for x in sequences
+        ]
+        outputs = [args.output / (x + '.npy') for x in sequences]
+    else:
+        inputs = [args.dat_file]
+        groundtruths = [args.groundtruth_dir]
+        outputs = [args.output]
 
-    segmentation = parse_tracks(
-        tracks_txt,
-        image_shape=(groundtruth.image_height, groundtruth.image_width),
-        track_label_size_space_separated=args.label_size_space_separated)
-    np.save(args.output_numpy, segmentation)
+    for input_path, groundtruth_path, output_path in zip(
+            inputs, groundtruths, outputs):
+        groundtruth = FbmsGroundtruth(groundtruth_path)
+
+        with open(input_path, 'r') as f:
+            tracks_txt = f.read()
+
+        segmentation = parse_tracks(
+            tracks_txt,
+            image_shape=(groundtruth.image_height, groundtruth.image_width),
+            track_label_size_space_separated=args.label_size_space_separated)
+        np.save(output_path, segmentation)
 
 
 if __name__ == "__main__":
