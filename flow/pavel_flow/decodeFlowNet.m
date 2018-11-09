dbaaa7cc372ad2a30efd73db5d882189b822ea15
Achal Dave <achalddave@live.com>
2018-11-09 10:44:37 -0500

    Update decodeFlowNet to work on arbitrary directories

--- flow/pavel_flow/decodeFlowNet.m
+++ flow/pavel_flow/decodeFlowNet.m
@@ -1,34 +1,41 @@
-function decodeFlowNetFBMS(set)
-    vids = dir(['/scratch/clear/ptokmako/datasets/FBMS/', set, '/Frames']);
-    for i = 3 : length(vids)
-        mkdir(['/scratch/clear/ptokmako/datasets/FBMS/', set, '/FlowNetFlowAngle/', vids(i).name]);
-        flows = dir(['/scratch/clear/ptokmako/datasets/FBMS/', set, '/FlowNetFlow/', vids(i).name, '/*.flo']);
-        for j = 1 : length(flows)
-            flow = readFloFile(['/scratch/clear/ptokmako/datasets/FBMS/', set, '/FlowNetFlow/', vids(i).name, '/', flows(j).name]);
-
-            baseVector = zeros(size(flow, 1), size(flow, 2), 2);
-            baseVector(:, :, 1) = 1;
-            angleField = acos(dot(flow, baseVector, 3) ./ (sqrt(flow(:, :, 1).^2 + flow(:, :, 2).^2) .* ones(size(flow, 1), size(flow, 2))));
-            magnitudes = sqrt(flow(:, :, 1).^2 + flow(:, :, 2).^2);
-
-            min_angle = min(angleField(:));
-            max_angle = max(angleField(:));
-
-            angleField = (angleField - min_angle) ./ (max_angle - min_angle);
-
-            frameName = strsplit(flows(j).name, '.');
-            imwrite(angleField, ['/scratch/clear/ptokmako/datasets/FBMS/', set, '/FlowNetFlowAngle/', vids(i).name, '/angleField_', frameName{1}, '.jpg']);
-
-            min_magnitude = min(magnitudes(:));
-            max_magnitude = max(magnitudes(:));
-
-            magnitudes = (magnitudes - min_magnitude) ./ (max_magnitude - min_magnitude);
-
-            imwrite(magnitudes, ['/scratch/clear/ptokmako/datasets/FBMS/', set, '/FlowNetFlowAngle/', vids(i).name, '/magField_', frameName{1}, '.jpg']);
-            fid = fopen(['/scratch/clear/ptokmako/datasets/FBMS/', set, '/FlowNetFlowAngle/', vids(i).name, '/minmax.txt'], 'a');
-            fprintf(fid, '%f %f %f %f\n', min_angle, max_angle, min_magnitude, max_magnitude);
-            fclose(fid);
-        end
+function decodeFlowNet(floDir, outputDir, overwrite)
+    if nargin < 3
+        overwrite = false;
     end
+    flows = dir([floDir, '/*.flo']);
+    for j = 1:length(flows)
+        frameName = strsplit(flows(j).name, '.');
+        outputAngle = [outputDir, '/angleField_', frameName{1}, '.jpg'];
+        outputMagnitude = [outputDir, '/magField_', frameName{1}, '.jpg'];
+        outputMinMax = [outputDir, '/minmax.txt'];
+        if ~overwrite && exist(outputAngle, 'file') && ...
+                         exist(outputMagnitude, 'file') && ...
+                         exist(outputMinMax, 'file')
+            continue
+        end
 
+        flow = readFloFile([floDir, '/', flows(j).name]);
+
+        baseVector = zeros(size(flow, 1), size(flow, 2), 2);
+        baseVector(:, :, 1) = 1;
+        angleField = acos(dot(flow, baseVector, 3) ./ (sqrt(flow(:, :, 1).^2 + flow(:, :, 2).^2) .* ones(size(flow, 1), size(flow, 2))));
+        magnitudes = sqrt(flow(:, :, 1).^2 + flow(:, :, 2).^2);
+
+        min_angle = min(angleField(:));
+        max_angle = max(angleField(:));
+
+        angleField = (angleField - min_angle) ./ (max_angle - min_angle);
+
+        imwrite(angleField, outputAngle);
+
+        min_magnitude = min(magnitudes(:));
+        max_magnitude = max(magnitudes(:));
+
+        magnitudes = (magnitudes - min_magnitude) ./ (max_magnitude - min_magnitude);
+
+        imwrite(magnitudes, outputMagnitude);
+        fid = fopen(outputMinMax, 'a');
+        fprintf(fid, '%f %f %f %f\n', min_angle, max_angle, min_magnitude, max_magnitude);
+        fclose(fid);
+    end
 end
